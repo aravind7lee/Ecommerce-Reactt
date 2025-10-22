0511c71d326d1bf8e9f3f689d5c5eca02b0830dc
aravind7Lee <aravindhhh5@gmail.com>
2025-10-22 15:57:59 +0530

    `Update package.json to remove "type": "module" and update server.js to use path.join for db.json path, and update api.js to use VITE_API_BASE_URL environment variable`

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const jsonServer = require('json-server');
+const path = require('path');
+
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults({
-  static: './build'
-});
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
+const middlewares = jsonServer.defaults();
 
 const port = process.env.PORT || 3001;
 
@@ -17,6 +17,7 @@ server.use((req, res, next) => {
 
 server.use(middlewares);
 server.use('/api', router);
+
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
